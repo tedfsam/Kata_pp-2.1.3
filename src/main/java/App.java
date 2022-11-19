63a6ec450a86febb9e7bfebededc69e952db18f7
Andrey Sterlyadev <sterlyadev.av@gmail.com>
2022-11-19 21:40:16 +0300

    Complit pp_2.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,21 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld beanHW =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHW.getMessage());
+
+        ApplicationContext applicationContext2 = new AnnotationConfigApplicationContext(AppConfig.class);
+        Cat beanCat =
+                (Cat) applicationContext2.getBean("cat");
+        System.out.println(beanCat.getMessage());
+
+        Cat beanCat1 =
+                (Cat) applicationContext2.getBean("cat");
+        System.out.println(beanCat1.getMessage());
+
+        System.out.println(bean == beanHW);
+        System.out.println(beanCat == beanCat1);
     }
 }
--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,16 +1,16 @@
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
 @Component
-public class HelloWorld {
- 
+public class Cat {
+
     private String message;
- 
+
     public String getMessage() {
         return message;
     }
- 
+
     public void setMessage(String message) {
         this.message = message;
     }
-     
 }
